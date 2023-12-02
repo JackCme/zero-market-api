4066dcfd4e20be38f1b3c0feeed82d97a26a2920
JackCme <pitou_106@naver.com>
2023-12-02 23:25:12 +0900

    fix: 비밀번호 정규표현식 빠진 특수문자 추가

--- src/main/java/com/example/marketapi/global/util/ValidationUtil.java
+++ src/main/java/com/example/marketapi/global/util/ValidationUtil.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class ValidationUtil {
     public static final String EMAIL_PATTERN = "^[a-zA-Z0-9_!#$%&'*+/=?{|}~^.-]+@[a-zA-Z0-9.-]+$";
-    public static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+    public static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()\\-_+=])[A-Za-z\\d!@#$%^&*()\\-_+=]{8,}$";
 
     public static boolean isValidEmail(String email) {
         return Pattern.compile(EMAIL_PATTERN).matcher(email).matches();
