c00b61f2a1da04189c3dd7627023431e60fc361e
WonJun Choi <jack@useb.co.kr>
2023-12-14 14:58:11 +0900

    feat: productService 구현

--- src/main/java/com/example/marketapi/domain/product/dto/ProductDto.java
+++ src/main/java/com/example/marketapi/domain/product/dto/ProductDto.java
@@ -1,10 +1,7 @@
 package com.example.marketapi.domain.product.dto;
 
 import com.example.marketapi.domain.product.entity.Product;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Builder
 @Getter
@@ -13,6 +10,7 @@ import lombok.NoArgsConstructor;
 public class ProductDto {
     private Long productId;
     private String name;
+    @Setter
     private Long inStock;
     private Long price;
 
@@ -24,4 +22,14 @@ public class ProductDto {
                 .price(product.getPrice())
                 .build();
     }
+
+    public static Product toEntity(ProductDto productDto) {
+        return Product.builder()
+                .productId(productDto.productId)
+                .name(productDto.name)
+                .inStock(productDto.inStock)
+                .price(productDto.getPrice())
+                .build();
+    }
+
 }
--- src/main/java/com/example/marketapi/domain/product/service/ProductService.java
+++ src/main/java/com/example/marketapi/domain/product/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.example.marketapi.domain.product.service;
 
 import com.example.marketapi.domain.product.dto.ProductDto;
+import com.example.marketapi.domain.product.entity.Product;
+import com.example.marketapi.domain.product.exception.ProductException;
 import com.example.marketapi.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,11 +16,19 @@ public class ProductService {
 
     @Transactional
     public ProductDto getProduct(Long productId) {
-        return null;
+        Product product = productRepository.findById(productId).orElseThrow(() -> new ProductException(ProductException.ErrorCode.PRODUCT_NOT_FOUND));
+        return ProductDto.fromEntity(product);
     }
 
     @Transactional
     public void decreaseProductStock(Long productId, Long byCount) {
+        ProductDto productDto = getProduct(productId);
+        if (productDto.getInStock() < byCount) {
+            throw new ProductException(ProductException.ErrorCode.INSUFFICIENT_STOCK_EXCEPTION);
+        }
+        productDto.setInStock(productDto.getInStock() - byCount);
+
+        productRepository.save(ProductDto.toEntity(productDto));
     }
 
 }
