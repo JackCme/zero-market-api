3fc6040d9f4a57126a2599cb60b7875a79a8cf2f
JackCme <pitou_106@naver.com>
2023-12-19 17:59:47 +0900

    fix: 주문 생성/취소 요청 응답 멤버변수 추가

--- src/main/java/com/example/marketapi/domain/order/api/model/Cancel.java
+++ src/main/java/com/example/marketapi/domain/order/api/model/Cancel.java
@@ -1,15 +1,19 @@
 package com.example.marketapi.domain.order.api.model;
 
+import com.example.marketapi.domain.order.dto.OrderInfoDto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 public class Cancel {
     @Getter
+    @AllArgsConstructor
     public static class Request {
-
+        private Long orderId;
     }
 
     @Getter
+    @AllArgsConstructor
     public static class Response {
-
+        private OrderInfoDto orderInfoDto;
     }
 }
--- src/main/java/com/example/marketapi/domain/order/api/model/Checkout.java
+++ src/main/java/com/example/marketapi/domain/order/api/model/Checkout.java
@@ -1,15 +1,19 @@
 package com.example.marketapi.domain.order.api.model;
 
+import com.example.marketapi.domain.order.dto.OrderInfoDto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 public class Checkout {
     @Getter
+    @AllArgsConstructor
     public static class Request {
-
+        private Long cartId;
     }
 
     @Getter
+    @AllArgsConstructor
     public static class Response {
-
+        private OrderInfoDto orderInfoDto;
     }
 }
